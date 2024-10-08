fa1b9c9d57aac9fe98725e3aaec9bf708d1da293
rowkee <rick.keenan10@gmail.com>
2024-10-08 20:47:51 +0100

    fixing vercel deployment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 // middleware
 app.use(express.json());
